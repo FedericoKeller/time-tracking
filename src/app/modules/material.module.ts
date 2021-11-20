356af5317f0404b2509bce9ae47863bfaa89974d
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-11-20 00:58:09 -0300

    feature(login): created register template and added password validation

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -4,6 +4,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   exports: [
     MatRippleModule,
@@ -11,6 +14,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatButtonModule,
     MatIconModule,
     MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
   ]
 })
 export class MaterialModule { }
--- src/app/modules/module-login/register/register.component.ts
+++ src/app/modules/module-login/register/register.component.ts
@@ -1,5 +1,13 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+const passwordErrorValidator: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+  const pass = group.get('password');
+  const confirmPass = group.get('passwordConfirm');
+
+  return pass?.value != confirmPass?.value ? { 'passwordError': true } : null;
+};
 
 @Component({
   selector: 'app-register',
@@ -7,10 +15,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+
+  constructor(private readonly fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: new FormControl('', [Validators.required]),
+      passwordsGroup: this.fb.group(
+        {
+          password: new FormControl('', [Validators.required]),
+          passwordConfirm: new FormControl('', [Validators.required]),
+        },
+        { validators: passwordErrorValidator }
+      ),
+    });
+  }
 
   ngOnInit() {
   }
-
 }
